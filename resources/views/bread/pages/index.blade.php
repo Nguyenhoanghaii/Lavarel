661620a270e4ecab424d2fcfc7ce6f0d9d6556c7
dvTinh99 <dvtinh.it@gmail.com>
2024-05-08 18:39:05 +0700

    show slide

--- resources/views/bread/pages/index.blade.php
+++ resources/views/bread/pages/index.blade.php
@@ -7,72 +7,25 @@
                     <div class="banner">
                         <ul>
                             <!-- THE FIRST SLIDE -->
-                            <li data-transition="boxfade" data-slotamount="20" class="active-revslide"
-                                style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
-                                <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined"
-                                    data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined"
-                                    data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined"
-                                    data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined"
-                                    data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined"
-                                    data-oheight="undefined">
-                                    <div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover"
-                                        data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined"
-                                        src="image/slide/banner1.jpg" data-src="image/slide/banner1.jpg"
-                                        style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('image/slide/banner1.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+                            @foreach ($slides as $slide)
+
+                                <li data-transition="boxfade" data-slotamount="20"
+                                    style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
+                                    <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined"
+                                        data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined"
+                                        data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined"
+                                        data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined"
+                                        data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined"
+                                        data-oheight="undefined">
+                                        <div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover"
+                                            data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined"
+                                            src="image/slide/{{ $slide->image }}" data-src="image/slide/{{ $slide->image }}"
+                                            style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('image/slide/{{ $slide->image }}'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
+                                        </div>
                                     </div>
-                                </div>
 
-                            </li>
-                            <li data-transition="boxfade" data-slotamount="20" class="active-revslide"
-                                style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
-                                <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined"
-                                    data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined"
-                                    data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined"
-                                    data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined"
-                                    data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined"
-                                    data-oheight="undefined">
-                                    <div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover"
-                                        data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined"
-                                        src="image/slide/banner2.jpg" data-src="image/slide/banner2.jpg"
-                                        style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('image/slide/banner2.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
-                                    </div>
-                                </div>
-
-                            <li data-transition="boxfade" data-slotamount="20" class="active-revslide"
-                                style="width: 100%; height: 100%; overflow: hidden; z-index: 18; visibility: hidden; opacity: 0;">
-                                <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined"
-                                    data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined"
-                                    data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined"
-                                    data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined"
-                                    data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined"
-                                    data-oheight="undefined">
-                                    <div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover"
-                                        data-bgposition="center center" data-bgrepeat="no-repeat" data-lazydone="undefined"
-                                        src="image/slide/banner3.jpg" data-src="image/slide/banner3.jpg"
-                                        style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('image/slide/banner3.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
-                                    </div>
-                                </div>
-
-                            </li>
-
-                            <li data-transition="boxfade" data-slotamount="20"
-                                class="active-revslide current-sr-slide-visible"
-                                style="width: 100%; height: 100%; overflow: hidden; visibility: inherit; opacity: 1; z-index: 20;">
-                                <div class="slotholder" style="width:100%;height:100%;" data-duration="undefined"
-                                    data-zoomstart="undefined" data-zoomend="undefined" data-rotationstart="undefined"
-                                    data-rotationend="undefined" data-ease="undefined" data-bgpositionend="undefined"
-                                    data-bgposition="undefined" data-kenburns="undefined" data-easeme="undefined"
-                                    data-bgfit="undefined" data-bgfitend="undefined" data-owidth="undefined"
-                                    data-oheight="undefined">
-                                    <div class="tp-bgimg defaultimg" data-lazyload="undefined" data-bgfit="cover"
-                                        data-bgposition="center center" data-bgrepeat="no-repeat"
-                                        data-lazydone="undefined" src="image/slide/banner4.jpg"
-                                        data-src="image/slide/banner4.jpg"
-                                        style="background-color: rgba(0, 0, 0, 0); background-repeat: no-repeat; background-image: url('image/slide/banner4.jpg'); background-size: cover; background-position: center center; width: 100%; height: 100%; opacity: 1; visibility: inherit;">
-                                    </div>
-                                </div>
-
-                            </li>
+                                </li>
+                            @endforeach
                         </ul>
                     </div>
                 </div>
